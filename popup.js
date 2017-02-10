c2d4cde46a02c58f32bbca8e3b92245894d72e96
tolyaganzin <tolyaganzin@mail.ru>
2017-02-10 17:01:00 +0200

    add styles for responce

--- popup.js
+++ popup.js
@@ -59,31 +59,77 @@ function onWindowLoad() {
           $.ajax({
              type: "POST",
              //the url where you want to sent the userName and password to
-             //url: 'http://192.168.0.145:5000/api/v2/json',
-             url: 'http://192.168.0.128:5000/api/v2/json',
+             url: 'http://192.168.0.118:4000/api/v2/json',
              data: req,
              success: function (data, textStatus, jqXHR) {
                //response from server success
-               if(data.img) {                 
-                 $('#message').text('');
-                 $('#message').append($("<img/>").attr("src", data.img));
+               $('#message').text('');
+               if(data.version == "old") {
+                 $('#message').append($("<img/>").attr("src", data.image));
                  console.log(textStatus);
                  console.log(data);
+               } else if (data.version == "new") {
+                 if (data.answers.length == 0) {
+                   $('#message').append($("<h1></h1>").text("go to: http://stackoverflow.com"));
+                 } else {
+                   var styles = {
+                     "border": "5px",
+                     "border-color": "#009406",
+                     "border-style": "solid",
+                     "border-radius": "7px",
+                     "padding": "10px",
+                     "color": "red"
+                   };
+                   for (var i = 0; i < data.answers.length; i++) {
+                     $('#message').append($("<pre></pre>").css(styles).text(data.answers[i]));
+                   }
+                 }
+                 console.log(textStatus);
+                 console.log(data);
+               } else {
+
+                 var styles = {
+                   "border": "5px",
+                   "border-color": "red",
+                   "border-style": "solid",
+                   "border-radius": "7px",
+                   "padding": "10px",
+                   "color": "red"
+                 };
+                 $('#message').append($("<pre></pre>").css(styles).text('invalid data'));
                }
              },
              error: function (jqXHR, textStatus, errorThrown) {
                //response from server error
+               var styles = {
+                 "border": "5px",
+                 "border-color": "red",
+                 "border-style": "solid",
+                 "border-radius": "7px",
+                 "padding": "10px",
+                 "color": "red"
+               };
+               $('#message').text('');
+               $('#message').append($("<pre></pre>").css(styles).text('no server connection'));
                console.log(textStatus);
              },
              contentType: "application/json",
              dataType: 'json',
              async: false
-             //json object to sent to the authentication url
           })
 
         } else {
           //if not valid page
-          alert("this page is not valid");
+          var styles = {
+            "border": "5px",
+            "border-color": "red",
+            "border-style": "solid",
+            "border-radius": "7px",
+            "padding": "10px",
+            "color": "red"
+          };
+          $('#message').text('');
+          $('#message').append($("<pre></pre>").css(styles).text('invalid page'));
         }
 
     });
